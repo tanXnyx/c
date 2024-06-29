cbf4fd5cc2550f64d480c6ddfae69f8c01abfffe
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2024-06-29 22:22:48 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if(age>=18){
+        printf("You are eligible to vote\n");
+    }
+    else{
+        printf("You are not eligible to vote\n");
+    }
+}
